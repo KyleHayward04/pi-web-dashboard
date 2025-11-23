7f91131f80fbf89d11fa8fc8672f273e6d4b79a0
hayward04 <kylehayward92@gmail.com>
2025-11-23 20:26:26 +0000

    Add CPU temp display to dashboard

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 #Pi-Web-Dashboard
 
+import gpiozero
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+	CPU_Temp = gpiozero.CPUTemperature()
+	CPU_Temp = round(CPU_Temp.temperature, 2)
+	mymessage = ("CPU Temp = " + str(CPU_Temp) + "\u00b0C")
+	return render_template("index.html", message=mymessage)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000)
